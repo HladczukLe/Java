e753c9d07d640fc42d99dc65de4546a7f9c24edc
Letícia Hladczuk <109438911+HladczukLe@users.noreply.github.com>
2022-09-25 19:01:29 -0300

    Create Exercise3.java

---
+++ Lista 1/Exercise3.java
@@ -0,0 +1,11 @@
+//Faça um programa que apresente alguma informação (pode ser seu nome) na tela.
+//Write a program that displays some information (it could be your name) on the screen.
+
+import javax.swing.JOptionPane;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");  // System.out = console.log
+        JOptionPane.showMessageDialog(null, "Hello, Via Lactea!"); //showMessageDialog = alert
+    }
+}
