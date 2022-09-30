9b069e703bd660b4486a3cbd57a7fee5ee20a1c7
Letícia Hladczuk <109438911+HladczukLe@users.noreply.github.com>
2022-09-29 23:36:50 -0300

    Update Condicional_natalidade.java

--- Condicional_natalidade.java
+++ Condicional_natalidade.java
@@ -1,4 +1,4 @@
-// Dado um país A, com 5.000.000 de habitantes e uma taxa de natalidade de 3% ao ano, e um país B com 7.000.000 de habitantes e uma taxa de natalidade de 2% ao ano, calcular e imprimir o tempo necessário para que a população do país A ultrapasse a população do país B
+// Dado um país A, com 5.000.000 de habitantes e uma taxa de natalidade de 3% ao ano, e um país B com 7.000.000 de habitantes e uma taxa de natalidade de 2% ao ano, calcular e imprimir o tempo necessário para que a população do país A ultrapasse a população do país B.
 
 public class natalidade {
     public static void main(String[] args) {
