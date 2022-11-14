52e1f493ea406a3e4d858179f6bcf1c70b358772
Letícia Hladczuk <109438911+HladczukLe@users.noreply.github.com>
2022-11-14 11:27:58 -0300

    Update Exercicio4.java

--- lista4/Exercicio4.java
+++ lista4/Exercicio4.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 import javax.swing.JOptionPane;
 
 /**
- * Exercicio4
+ * Questão 4: Implemente um programa que auxilie os profissionais de química a estimarem a quantidade de substância radioativa que permanece no decorrer do tempo. O usuário deve entrar a quantidade inicial da substância, a taxa de desintegração da substância e o tempo que se passou; e deve apresentar ao usuário a quantidade que ainda sobrou da substância. Utilize a seguinte expressão Q = Qo × e^−rt
  */
 public class Exercicio4 {
     public static void main(String[] args) {
